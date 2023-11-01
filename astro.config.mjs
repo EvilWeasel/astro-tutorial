0c4ffe1ae8f6fa3e33587ad3c12774f32faec9ba
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-11-01 18:51:22 +0100

    added dynamic routing; astro.glob; rss-feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://evilweasel-blog.netlify.app'
+});
