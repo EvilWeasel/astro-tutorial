7364c01128030aa82adc6146040738509741d1fe
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-11-02 09:56:05 +0100

    refactored from astro.glob to contentCollections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
-    title: 'EvilWeasel | Blog',
-    description: 'My journey learning Astro',
+    title: "EvilWeasel | Blog",
+    description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
